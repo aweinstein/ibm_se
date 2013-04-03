2e72463d57fa8d7ba784a0d45741d0e04d9c94a8
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2013-04-03 08:42:05 -0300

    Add USB serial loopback.

---
+++ usb/Descriptors.d
@@ -0,0 +1,270 @@
+Descriptors.o: Descriptors.c Descriptors.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/USB.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/../../Common/Common.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/../../Common/Architectures.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/../../Common/BoardTypes.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/../../Common/ArchitectureSpecific.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/../../Common/CompilerSpecific.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/../../Common/Attributes.h \
+ Config/LUFAConfig.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/../../Common/Endianness.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/USBMode.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/../../../Common/Common.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/USBTask.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/USBMode.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/USBController.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBMode.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Events.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBMode.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBTask.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/USBInterrupt_AVR8.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../../../../Common/Common.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBMode.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../Events.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBController.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Device.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../StdDescriptors.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Events.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Endpoint.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/Endpoint_AVR8.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBTask.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBInterrupt.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/Device_AVR8.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../StdDescriptors.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../Endpoint.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Endpoint.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../DeviceStandardReq.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../StdRequestType.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBTask.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBController.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../EndpointStream.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/EndpointStream_AVR8.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/Events.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/StdRequestType.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/StdDescriptors.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/DeviceStandardReq.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/Events.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/StdDescriptors.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/ConfigDescriptors.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/HostStandardReq.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/USBController.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/USBInterrupt.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/Device.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/Endpoint.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/DeviceStandardReq.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/EndpointStream.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/AndroidAccessoryClass.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/../Core/USBMode.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/AudioClass.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/AudioClassDevice.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../../USB.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/AudioClassCommon.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/../../Core/StdDescriptors.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/CDCClass.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/CDCClassDevice.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/CDCClassCommon.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/HIDClass.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/HIDClassDevice.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/HIDClassCommon.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/HIDParser.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/../../../../Common/Common.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/HIDReportData.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/HIDClassCommon.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/MassStorageClass.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/MassStorageClassDevice.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/MassStorageClassCommon.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/MIDIClass.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/MIDIClassDevice.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/MIDIClassCommon.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/AudioClassCommon.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/PrinterClass.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/RNDISClass.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/RNDISClassDevice.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/RNDISClassCommon.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/CDCClassCommon.h \
+ /home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/StillImageClass.h
+
+Descriptors.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/USB.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/../../Common/Common.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/../../Common/Architectures.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/../../Common/BoardTypes.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/../../Common/ArchitectureSpecific.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/../../Common/CompilerSpecific.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/../../Common/Attributes.h:
+
+Config/LUFAConfig.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/../../Common/Endianness.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/USBMode.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/../../../Common/Common.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/USBTask.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/USBMode.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/USBController.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBMode.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Events.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBMode.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBTask.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/USBInterrupt_AVR8.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../../../../Common/Common.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBMode.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../Events.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBController.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Device.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../StdDescriptors.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Events.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Endpoint.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/Endpoint_AVR8.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBTask.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBInterrupt.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/Device_AVR8.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../StdDescriptors.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/../Endpoint.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../Endpoint.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../DeviceStandardReq.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../StdRequestType.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBTask.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../USBController.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../EndpointStream.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/AVR8/../AVR8/EndpointStream_AVR8.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/Events.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/StdRequestType.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/StdDescriptors.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/DeviceStandardReq.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/Events.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/StdDescriptors.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/ConfigDescriptors.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/HostStandardReq.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/USBController.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/USBInterrupt.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/Device.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/Endpoint.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/DeviceStandardReq.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Core/EndpointStream.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/AndroidAccessoryClass.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/../Core/USBMode.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/AudioClass.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/AudioClassDevice.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../../USB.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/AudioClassCommon.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/../../Core/StdDescriptors.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/CDCClass.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/CDCClassDevice.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/CDCClassCommon.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/HIDClass.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/HIDClassDevice.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/HIDClassCommon.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/HIDParser.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/../../../../Common/Common.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/HIDReportData.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/HIDClassCommon.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/MassStorageClass.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/MassStorageClassDevice.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/MassStorageClassCommon.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/MIDIClass.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/MIDIClassDevice.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/MIDIClassCommon.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/AudioClassCommon.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/PrinterClass.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/RNDISClass.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/RNDISClassDevice.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/RNDISClassCommon.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/Device/../Common/CDCClassCommon.h:
+
+/home/ajw/local/lufa/LUFA/../LUFA/Drivers/USB/Class/StillImageClass.h:
